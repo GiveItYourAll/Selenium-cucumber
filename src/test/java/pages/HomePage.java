972c34903f54d3e94b0d90a41d3e1e3689da4bcf
Dmytro Chobotar <82722335+dmytro-ch21@users.noreply.github.com>
2024-05-21 16:50:56 -0400

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -9,6 +9,8 @@ public class HomePage {
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
+    // you dont have the Singleton implemented yet so multiple objects created in same step definition will fail.
+    // move this locator to LoginPage for now
     @FindBy(id = "welcome")
     public WebElement welcomeElement;
 }
